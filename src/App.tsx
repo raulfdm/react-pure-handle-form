f27e5c298a1708e28ab476bb78346f724dd9d4d7
Raul Melo <melo.raulf@gmail.com>
2021-02-27 07:12:34 +0100

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, FormEvent } from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Input Tests</h1>
       </header>
+      <main>
+        <fieldset
+          onChange={(e: FormEvent<HTMLFieldSetElement>) => {
+            console.log((e.target as HTMLInputElement).name);
+          }}
+          onFocus={(e: FormEvent<HTMLFieldSetElement>) => {
+            const { name } = e.target as HTMLInputElement;
+            console.log({ name });
+          }}
+        >
+          <input type="text" name="card" placeholder="0000 0000 0000 0000" />
+          <input type="text" name="date" placeholder="MM/YY" />
+          <input type="text" name="name" placeholder="John Doe" />
+        </fieldset>
+      </main>
     </div>
   );
 }
